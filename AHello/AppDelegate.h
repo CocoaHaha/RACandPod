9966f762ca4a42174ea4722eb1d03a31b63c9bdc
石向锋 <SXF@shixiangfengdeMacBook-Pro.local>
2017-05-09 10:12:35 +0800

    Initial Commit

---
+++ AHello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AHello
+//
+//  Created by 石向锋 on 2017/5/9.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AHello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AHello
+//
+//  Created by 石向锋 on 2017/5/9.
+//  Copyright © 2017年 CocoHaHa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
